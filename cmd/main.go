6e9c51f7694b0a71979d27bf8cda5839442b8911
Don Brower <dbrower@nd.edu>
2019-09-23 17:14:28 -0400

    adding fedora harvesting code

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/dbrower/fut"
+)
+
+func main() {
+	fpath := os.Getenv("FEDORA_PATH")
+	if fpath == "" {
+		log.Println("FEDORA_PATH not set")
+		return
+	}
+	fedora := fut.NewRemote(fpath)
+	t := time.Now()
+	t = t.Add(-5 * 24 * time.Hour)
+
+	fut.HarvestCurateObjects(fedora, t)
+}
--- fut.go
+++ fut.go
@@ -1,4 +1,4 @@
-package main
+package fut
 
 import (
 	"html/template"
